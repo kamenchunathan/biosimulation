a7611eb4586fec30ede53cd450950e6b5acc44fd
Nathan <kamenchunathan@gmail.com>
2021-12-02 14:05:48 +0300

    organism.rs: add more detail to the organism genome
    Neuron types were added to the organism gene and a genome type created. Related changes were also made

--- src/organism.rs
+++ src/organism.rs
@@ -1,27 +1,60 @@
-// Organism brain which is a neural network
+// ------------------------ ORGANISM GENOME --------------------------------
+#[derive(Clone, Debug)]
+enum NeuronType {
+    Sensor(u16),        // always a source
+    Neuron(u16),        // may be a sink or source
+    Action(u16),        // always a sink
+}
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
+struct Gene {
+    source: NeuronType,
+    sink: NeuronType,
+    weight: f32,
+    bias: Option<f32>,
+    // does not specify the activation function for now but may do so in future
+    // activation_function: fn(f32) -> (),
+}
+
+impl Gene {
+    fn random() -> Self {
+        Self {
+            source: NeuronType::Sensor(0),
+            sink: NeuronType::Sensor(0),
+            weight: 0.0,
+            bias: None,
+            // activation_function: |a|{},
+        }
+    }
+}
+
+type Genome = Vec<Gene>;
+
+// ------------------------ ORGANISM BRAIN ---------------------------------
+//  A neural network representing how the organism is able to make decisions on where to go
+#[derive(Clone, Debug)]
 struct Brain;
 
 impl Brain {
-    pub fn random() -> Self {
-        // TODO: create a random brain
-        //  really a stub function that's never needed as brain is always created from a gene
+    pub fn from_genome(_genome: &Genome) -> Self {
         Self
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+// ------------------------------- ORGANISM  -------------------------------
+#[derive(Clone, Debug)]
 pub struct Organism {
-    gene: u8,
+    genome: Genome,
     brain: Brain,
 }
 
 impl Default for Organism {
     fn default() -> Self {
+        let genome = vec![Gene::random()];
+        let brain = Brain::from_genome(&genome);
         Self {
-            gene: 0,
-            brain: Brain::random(),
+            genome,
+            brain,
         }
     }
 }
