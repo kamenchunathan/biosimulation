2ac4372169017b149bd316994668d85fc71eb0e7
Nathan <kamenchunathan@gmail.com>
2021-12-16 02:35:02 +0300

    feat: Expand NeuronTypes for genome
    Neuron types were expanded to store more information depending on
    type of neuron. e.g. biases were added for interneurons, and sensor
    types for sensor neurons similarly for action neurons

--- src/organism.rs
+++ src/organism.rs
@@ -1,9 +1,46 @@
 // ------------------------ ORGANISM GENOME --------------------------------
 #[derive(Clone, Debug)]
+enum SensorType {
+    LocationX,
+    LocationY,
+    Age,
+    Random,
+    Oscillator,
+}
+
+#[derive(Clone, Debug)]
+struct SensorData {
+    id: u32,
+    sensor_type: SensorType,
+}
+
+
+#[derive(Clone, Debug)]
+struct InterNeuronData {
+    id: u32,
+    bias: f64,
+}
+
+#[derive(Clone, Debug)]
+enum ActionType {
+    MoveX,
+    MoveY,
+    MoveForward,
+    MoveRandom,
+    MoveReverse,
+}
+
+#[derive(Clone, Debug)]
+struct ActionData {
+    id: u32,
+    action_type: ActionType,
+}
+
+#[derive(Clone, Debug)]
 enum Neuron {
-    Sensor,             // always a source
-    Neuron(u16),        // may be a sink or source
-    Action(u16),        // always a sink
+    Sensor(SensorData),
+    InterNeuron(InterNeuronData),
+    Action(ActionData),
 }
 
 #[derive(Clone, Debug)]
@@ -11,19 +48,24 @@ struct Gene {
     source: Neuron,
     sink: Neuron,
     weight: f32,
-    bias: Option<f32>,
-    // does not specify the activation function for now but may do so in future
-    // activation_function: fn(f32) -> (),
 }
 
 impl Gene {
     fn random() -> Self {
         Self {
-            source: Neuron::Sensor,
-            sink: Neuron::Sensor,
+            source: Neuron::Sensor(
+                SensorData {
+                    id: 0,
+                    sensor_type: SensorType::Random
+                }
+            ),
+            sink: Neuron::Action(
+                ActionData{
+                    id: 1,
+                    action_type: ActionType::MoveRandom
+                }
+            ),
             weight: 0.0,
-            bias: None,
-            // activation_function: |a|{},
         }
     }
 }
